33420330efd8f7d76272a9b9ea3c023f9136d45d
Emeline Wulstecke <emeline.wulstecke@gmail.com>
2024-12-13 17:08:31 +0100

    doc: gitignore added

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,14 @@
 const express = require("express");
-
-const app = express();
+const mongoose = require('mongoose');
 const dotenv = require("dotenv");
 dotenv.config();
+const path = require('path');
+
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+  const app = express();
 
 module.exports = app;
