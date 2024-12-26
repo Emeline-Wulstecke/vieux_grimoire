444ba7214345ed773fb516f62c5de2d0cb7553de
Emeline Wulstecke <emeline.wulstecke@gmail.com>
2024-12-26 18:13:22 +0100

    wip: route and controller user added

---
+++ backend/route/user.route.js
@@ -0,0 +1,11 @@
+"use strict"
+
+const express = require("express");
+const router = express.Router();
+
+const userCtrl = require("../controller/user.controller");
+
+router.post("/signup", userCtrl.signup);
+router.post("/login", userCtrl.login);
+
+module.exports = router;
