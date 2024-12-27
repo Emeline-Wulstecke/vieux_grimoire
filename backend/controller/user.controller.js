509188329279500dfbc5f686fc56f054fdfbad42
Emeline-Wulstecke <emeline.wulstecke@gmail.com>
2024-12-27 10:26:55 +0100

    fix: jwt instead of jtw jeton

--- backend/controller/user.controller.js
+++ backend/controller/user.controller.js
@@ -20,25 +20,38 @@ exports.signup = (req, res, next) => {
 };
 
 exports.login = (req, res, next) => {
+  console.log("Requête reçue pour login :", req.body);
+
   User.findOne({ email: req.body.email })
     .then((user) => {
       if (!user) {
+        console.log("Utilisateur non trouvé !");
         return res.status(401).json({ error: "Utilisateur non trouvé !" });
       }
+      console.log("Utilisateur trouvé :", user);
+
       bcrypt
         .compare(req.body.password, user.password)
         .then((valid) => {
           if (!valid) {
+            console.log("Mot de passe incorrect !");
             return res.status(401).json({ error: "Mot de passe incorrect !" });
           }
+          console.log("Authentification réussie !");
           res.status(200).json({
             userId: user._id,
-            token: jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+            token: jwt.sign({ userId: user._id }, process.env.JWT_SECRET , {
               expiresIn: "24h",
             }),
           });
         })
-        .catch((error) => res.status(500).json({ error }));
+        .catch((error) => {
+          console.error("Erreur lors de la comparaison :", error);
+          res.status(500).json({ error });
+        });
     })
-    .catch((error) => res.status(500).json({ error }));
+    .catch((error) => {
+      console.error("Erreur lors de la recherche de l'utilisateur :", error);
+      res.status(500).json({ error });
+    });
 };
